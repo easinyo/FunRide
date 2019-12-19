9c0ae5a107d1407af8352dcb58a53c0a20a7c4de
Brahim kanouche <31965027+kanouche@users.noreply.github.com>
2019-12-19 12:01:10 -0500

    documented signup presenter

--- app/src/main/java/com/dubinostech/rideshareapp/presenter/interfaces/SignUpPresenterInterface.java
+++ app/src/main/java/com/dubinostech/rideshareapp/presenter/interfaces/SignUpPresenterInterface.java
@@ -1,3 +1,8 @@
+/**
+* Interface SignUpPresenterInterface to organize the signup presenter
+* To be implemented by SignupPresenter
+* Has CallSignup method that takes an object user
+*/
 package com.dubinostech.rideshareapp.presenter.interfaces;
 
 import com.dubinostech.rideshareapp.data.User;
