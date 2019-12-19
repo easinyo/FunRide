be5ec4cfb4c256d3a8b7de3b00f00cd9e2942838
Brahim kanouche <31965027+kanouche@users.noreply.github.com>
2019-12-19 12:00:14 -0500

    update headings

--- app/src/main/java/com/dubinostech/rideshareapp/presenter/interfaces/LoginPresenterInterface.kt
+++ app/src/main/java/com/dubinostech/rideshareapp/presenter/interfaces/LoginPresenterInterface.kt
@@ -1,3 +1,8 @@
+/**
+* Interface LoginPresenterInterface to organize the login presenter
+* To be implemented by LoginPresenter
+* Has CallLogin method that takes username and password
+*/
 package com.dubinostech.rideshareapp.presenter.interfaces
 
 interface LoginPresenterInterface {
