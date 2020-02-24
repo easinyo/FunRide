c326bb5627025035ace24bba0418ab5e7c4583cb
Emmanuel Asinyo <34837163+emmanueldubien@users.noreply.github.com>
2020-02-23 19:34:21 -0500

    Testing & Profile

---
+++ app/src/androidTest/java/com/dubinostech/rideshareapp/BaseInstrumentedTest.kt
@@ -0,0 +1,42 @@
+package com.dubinostech.rideshareapp
+
+import android.arch.lifecycle.ViewModel
+import android.support.test.InstrumentationRegistry
+import com.nhaarman.mockito_kotlin.KStubbing
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.wabadaba.dziennik.di.ViewModelFactory
+import com.wabadaba.dziennik.ui.GPServicesChecker
+import org.junit.Before
+import org.junit.Rule
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+
+abstract class BaseInstrumentedTest {
+    @Suppress("unused")
+    @get:Rule
+    val rule = EspressoDaggerMockRule()
+
+    @Mock lateinit var viewModelFactory: ViewModelFactory
+
+    @Mock lateinit var servicesChecker: GPServicesChecker
+    @Before
+    fun setup() {
+        `when`(servicesChecker.check(any()))
+            .doReturn(true)
+    }
+
+    inline fun <reified T : ViewModel> mockViewModel(stubbing: KStubbing<T>.(T) -> Unit): T {
+        val viewModelMock = mock<T>(stubbing = stubbing)
+
+        `when`(viewModelFactory.create(T::class.java)).thenReturn(viewModelMock)
+        return viewModelMock
+    }
+
+    companion object {
+        fun getApp(): MainApplication = InstrumentationRegistry.getInstrumentation()
+            .targetContext
+            .applicationContext as MainApplication
+    }
+}
---
+++ app/src/androidTest/java/com/dubinostech/rideshareapp/LoginActivityTest.kt
@@ -0,0 +1,76 @@
+package com.dubinostech.rideshareapp
+
+
+import android.app.Activity
+import android.app.Instrumentation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.withId
+
+import com.dubinostech.rideshareapp.ui.activities.LoginActivity
+import com.nhaarman.mockito_kotlin.any
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.times
+import com.wabadaba.dziennik.api.*
+import com.wabadaba.dziennik.ui.MainActivity
+import com.wabadaba.dziennik.ui.login.LoginActivity
+import com.wabadaba.dziennik.vo.Me
+import com.wabadaba.dziennik.vo.MeEntity
+import io.reactivex.Observable
+import io.reactivex.Single
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
+class LoginActivityTest : BaseInstrumentedTest() {
+
+    @get:Rule private val activityRule = IntentsTestRule(LoginActivity::class.java, false, false)
+    @Mock private lateinit var userRepository: UserRepository
+    @Mock private lateinit var apiClientFactory: APIClientFactory
+    @Mock private lateinit var loginClient: LoginClient
+
+    private val username = "username"
+    private val password = "password"
+
+
+    @Test
+    fun shouldLogIn() {
+        val authInfo = AuthInfo("AToken", "RToken")
+        `when`(loginClient.login(username, password)).thenReturn(Single.just(authInfo))
+        val login = "testlogin"
+        val me = MeEntity().apply {
+            account.setLogin(login)
+            account.setFirstName("Tomasz")
+            account.setLastName("Problem")
+            user.setFirstName("Tomek")
+            user.setLastName("Problem")
+            account.setGroupId(5)
+        } as Me
+        val mockApiClient = mock<APIClient> {
+            on { fetchEntities(Me::class) } doReturn Observable.just(me)
+        }
+        `when`(apiClientFactory.create(any(), any())).thenReturn(mockApiClient)
+        activityRule.launchActivity(null)
+        val packageName = MainApplication::class.java.`package`.name
+        val result = Instrumentation.ActivityResult(Activity.RESULT_OK, null)
+
+        Intents.intending(toPackage(packageName)).respondWith(result)
+        onView(withId(R.id.usernameEditText)).perform(typeText(username))
+        onView(withId(R.id.passwordEditText)).perform(typeText(password))
+        Espresso.closeSoftKeyboard()
+        onView(withId(R.id.logInButton)).perform(click())
+
+        Intents.intended(allOf(toPackage(packageName), hasComponent(MainActivity::class.java.name)))
+        verify(userRepository, times(1)).addUser(FullUser(
+            login,
+            "Tomasz", "Problem",
+            "Tomek", "Problem",
+            5,
+            authInfo))
+        Intents.release()
+    }
+
+}}
