ca9690179b01c4ccfc9bdc672b7efa96f9101a58
Emmanuel Asinyo <34837163+emmanueldubien@users.noreply.github.com>
2020-03-09 16:19:48 -0400

    Adding the dependacies

---
+++ app/src/main/java/com/dubinostech/rideshareapp/repository/Data/LoggedUser.java
@@ -0,0 +1,37 @@
+package com.dubinostech.rideshareapp.repository.Data;
+
+public class LoggedUser {
+    public static String token = "";
+    public static String name = "Emmanuel Asinyo";
+    public static String email = "easin081@uottawa.ca";
+
+    public LoggedUser(String token, String name, String email) {
+        this.token = token;
+        this.name = name;
+        this.email = email;
+    }
+
+    public static String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
