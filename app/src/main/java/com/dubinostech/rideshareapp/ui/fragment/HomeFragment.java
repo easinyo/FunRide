99787853124d160de79cbe4272ef22e5877c2035
Brahim kanouche <bkano020@uottawa.ca>
2020-03-22 19:07:01 -0400

    adopted auto completed used for post for consistency

--- app/src/main/java/com/dubinostech/rideshareapp/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/dubinostech/rideshareapp/ui/fragment/HomeFragment.java
@@ -3,6 +3,7 @@ package com.dubinostech.rideshareapp.ui.fragment;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.location.Address;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,11 +12,11 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.dubinostech.rideshareapp.R;
@@ -23,11 +24,19 @@ import com.dubinostech.rideshareapp.model.loginModel.SearchModel;
 import com.dubinostech.rideshareapp.presenter.SearchRidePresenter;
 import com.dubinostech.rideshareapp.repository.Api.Responses.SearchResponse;
 import com.dubinostech.rideshareapp.repository.ErrorHandler.ErrorCode;
+import com.dubinostech.rideshareapp.repository.Libraries.Utils;
 import com.dubinostech.rideshareapp.ui.activities.ReservationActivity;
 import com.dubinostech.rideshareapp.ui.view.SearchListAdapter;
 import com.dubinostech.rideshareapp.ui.view.SearchView;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
@@ -41,28 +50,29 @@ import static android.widget.Toast.makeText;
 public class HomeFragment extends Fragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener, SearchView {
 
     private Button searchRide;
-    private EditText departure;
-    private EditText arrival;
+    private TextView departureDateLabel;
+    private TextView departureTimeLabel;
     private TextView date;
     private TextView error;
     private Calendar myCalendar;
     private ListView listview;
-    private SearchListAdapter searchListAdapter;
 
-
-
-    private static final int REQUEST_CODE_GET_DEPARTURE_LOCATION= 1;
-    private static final int REQUEST_CODE_GET_ARRIVAL_LOCATION= 2;
     private ProgressDialog progressDialog;
 
+    PlacesClient placesClient;
+
+    String departure_city;
+    String arrival_city;
+
+    final String API_KEY = "AIzaSyByniyl8kvVAZ0eGBbPxyUVzVg9jgV5XfA";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup groupView = (ViewGroup)inflater.inflate(R.layout.fragment_home, container, false);
 
-        departure = groupView.findViewById(R.id.departure);
-        arrival = groupView.findViewById(R.id.arrival);
         searchRide = groupView.findViewById(R.id.search);
+        departureDateLabel = groupView.findViewById(R.id.departure_date_label);
+        departureTimeLabel = groupView.findViewById(R.id.departure_time_label);
         date = groupView.findViewById(R.id.date);
         error = groupView.findViewById(R.id.error);
         myCalendar = Calendar.getInstance();
@@ -75,8 +85,59 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
 
         date.setOnClickListener(this);
         searchRide.setOnClickListener(this);
-        searchRide.setEnabled(true);
 
+        // Setup Places Client
+        if (!Places.isInitialized()) {
+            Places.initialize(getActivity(), API_KEY);
+        }
+        // Retrieve a PlacesClient (previously initialized - see MainActivity)
+        placesClient = Places.createClient(getActivity());
+
+        final AutocompleteSupportFragment autocompleteSupportFragmentArrival =
+                (AutocompleteSupportFragment)
+                        getChildFragmentManager().findFragmentById(R.id.autocomplete_arrival_location);
+        autocompleteSupportFragmentArrival.getView().setBackgroundColor(ContextCompat.getColor(this.getContext(), R.color.colorPrimary));
+
+
+        autocompleteSupportFragmentArrival.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG,Place.Field.ADDRESS));
+
+        autocompleteSupportFragmentArrival.setOnPlaceSelectedListener(
+                new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(Place place) {
+                        Address address = Utils.placeToAddress(place, getContext());
+
+                        arrival_city = address.getLocality()+ ", " + address.getAdminArea() + ", " + address.getCountryName();
+
+                    }
+                    @Override
+                    public void onError(Status status) {
+                    }
+                });
+
+        final AutocompleteSupportFragment autocompleteSupportFragmentDeparture =
+                (AutocompleteSupportFragment)
+                        getChildFragmentManager().findFragmentById(R.id.autocomplete_departure_city);
+        autocompleteSupportFragmentDeparture.getView().setBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorPrimary));
+
+        autocompleteSupportFragmentDeparture.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG,Place.Field.ADDRESS));
+
+        autocompleteSupportFragmentDeparture.setOnPlaceSelectedListener(
+                new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(Place place) {
+                        Address address = Utils.placeToAddress(place, getContext());
+
+                        departure_city = address.getLocality()+ ", " + address.getAdminArea() + ", " + address.getCountryName();
+
+                    }
+
+                    @Override
+                    public void onError(Status status) {
+                    }
+                });
+
+        searchRide.setEnabled(true);
         return groupView;
     }
 
@@ -94,21 +155,20 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
     }
 
     private void searchRide() {
-        String departureStr = departure.getText().toString();
-        String arrivalStr = arrival.getText().toString();
+
         String dateStr = date.getText().toString();
 
         this.progressDialog = new ProgressDialog(getActivity());
 
-        if (departureStr.isEmpty()) {
+        if (departure_city.isEmpty()) {
             displayToast("Departure city field empty");
-        } else if (arrivalStr.isEmpty()) {
+        } else if (arrival_city.isEmpty()) {
             displayToast("Arrival city field empty");
         } else if (dateStr.isEmpty()) {
             displayToast("Date field empty");
         } else {
             SearchRidePresenter presenter = new SearchRidePresenter(this, new SearchModel());
-            presenter.callSearch(departureStr, arrivalStr, dateStr);
+            presenter.callSearch(departure_city, arrival_city, dateStr);
         }
     }
 
@@ -143,26 +203,11 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
 
     @Override
     public void searchSuccess(List<SearchResponse> rides) {
-        progressDialog.dismiss();
-        //        if(rides.size() > 0) {
-//            final Intent intent = new Intent(getContext(), SearchResultActivity.class);
-//            ArrayList<Integer> ridesList = new ArrayList<Integer>();
-//            Object[] array = rides.toArray();
-//            if (array == null) {
-//                //error = "No trips found.";
-//            } else {
-//                for (int i = 0; i < array.length ; i++) {
-//                    //For each integer, adds it to list
-//                    ridesList.add((Integer) array[i]);
-//                }
-//                Bundle extras = new Bundle();
-//                //extras.putString("EXTRA_USERNAME", eusername);
-//                extras.putIntegerArrayList("rides", ridesList);
-//                intent.putExtras(extras);
-//                startActivity(intent);
-//            }
-//        }
-
+        hideLoading();
+         if(rides==null || rides.size()==0){
+            error.setText("Sorry, no rides found!");
+            error.setVisibility(View.VISIBLE);
+         }
         SearchListAdapter adapter = new SearchListAdapter(getContext(), 0, rides);
 
         listview.setAdapter(adapter);
@@ -189,11 +234,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener, Date
 
     @Override
     public void searchFailure(String errMsg) {
+        hideLoading();
         Toast.makeText(getActivity(), errMsg, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void searchFailure(ErrorCode error) {
+        hideLoading();
         if (error.getId() == 0) {
             Toast.makeText(
                     getActivity(),
